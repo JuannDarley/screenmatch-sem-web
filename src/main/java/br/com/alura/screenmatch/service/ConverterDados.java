fb86376abb16e3fd598cba3a1c0de1f15733499d
Juan Darley <juandarley99@gmail.com>
2025-12-13 16:31:45 -0300

    inicialized project

---
+++ src/main/java/br/com/alura/screenmatch/service/ConverterDados.java
@@ -0,0 +1,16 @@
+package br.com.alura.screenmatch.service;
+
+
+import tools.jackson.databind.ObjectMapper;
+
+public class ConverterDados implements IConverteDados {
+    private ObjectMapper mapper = new ObjectMapper();
+
+    @Override
+    public <T> T obterDados(String json, Class<T> classe) {
+
+        return mapper.readValue(json, classe);
+
+
+    }
+}
