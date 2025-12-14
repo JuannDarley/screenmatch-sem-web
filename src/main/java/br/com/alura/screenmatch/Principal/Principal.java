e536d815f17fca695b9b1ea519d0c3d8105fbe0a
Juan Darley <juandarley99@gmail.com>
2025-12-14 12:56:58 -0300

    add map for dadosEpisodios

--- src/main/java/br/com/alura/screenmatch/Principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/Principal/Principal.java
@@ -7,8 +7,10 @@ import br.com.alura.screenmatch.service.ConsumoApi;
 import br.com.alura.screenmatch.service.ConverterDados;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Principal {
 
@@ -41,13 +43,26 @@ public class Principal {
         temporadas.forEach(System.out::println);
 
 
-        /*for(int i = 0; i < dados.totalTemporadas(); i++ ){
+
+
+        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+
+        List<DadosEpisodio> dadosEpisodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream())
+                .collect(Collectors.toList());
+
+        dadosEpisodios.stream()
+                .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
+                .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
+                .limit(5)
+                .forEach(System.out::println);
+    }
+}
+
+// Exemplo
+ /*for(int i = 0; i < dados.totalTemporadas(); i++ ){
            List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
            for(int j = 0; j < episodiosTemporada.size(); j++ ){
                System.out.println(episodiosTemporada.get(j).titulo());
            }
         }*/
-
-        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
-    }
-}
